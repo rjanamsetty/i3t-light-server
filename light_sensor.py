3b947ce1bb49e3bc53c927cd7ee25a37dda552c8
Ritvik Janamsetty <rj160@duke.edu>
2022-04-11 16:37:19 -0400

    Added back OS

--- light_sensor.py
+++ light_sensor.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'lib')
+if os.path.exists(libdir):
+    sys.path.append(libdir)
+
 from waveshare_TSL2591 import TSL2591
 
 sensor = TSL2591.TSL2591()
