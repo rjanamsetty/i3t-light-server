451b9a5fb1ebd7abcbcbecd981ab4047592cb123
Ritvik Janamsetty <rj160@duke.edu>
2022-04-11 22:40:21 -0400

    finished code

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, request
-import lifx
+from csv import DictWriter
 
+from flask import Flask, request
+from lifx import Lifx
+import light_sensor
 
 app = Flask(__name__)
+lifx = Lifx()
 
 
 @app.route('/toggle', methods=['POST'])
@@ -41,6 +44,21 @@ def brightness():
         else:
             lifx.set_brightness(value)
             return 'ok', 200
+    except:
+        return 'Bad Gateway', 502
+
+
+@app.route('/results', methods=['POST'])
+def results():
+    time = request.args.get('time', default=0, type=int)
+    try:
+        headersCSV = ['Time', 'Brightness', 'Lux']
+        with open('data.csv', 'a', newline='') as f_object:
+            dictwriter_object = DictWriter(f_object, fieldnames=headersCSV)
+            list_data = {"Time": time, 'Brightness': lifx.get_brightness(), 'Lux': light_sensor.get_lux()}
+            dictwriter_object.writerow(list_data)
+            f_object.close()
+        return 'ok', 200
     except:
         return 'Bad Gateway', 502
 
--- lifx.py
+++ lifx.py
@@ -5,54 +5,57 @@ from lifxlan import LifxLAN
 lifx = LifxLAN(1)
 
 
-def on():
-    """
-    Turns on all lights
-    :return: None
-    """
-    lifx.set_power_all_lights("on", rapid=True)
+class Lifx:
+    bright = 0
 
+    def on(self):
+        """
+        Turns on all lights
+        :return: None
+        """
+        lifx.set_power_all_lights("on", rapid=True)
 
-def off():
-    """
-    Turns off all lights
-    :return: None
-    """
-    lifx.set_power_all_lights("off", rapid=True)
+    def off(self):
+        """
+        Turns off all lights
+        :return: None
+        """
+        lifx.set_power_all_lights("off", rapid=True)
 
+    def power_state(self):
+        """
+        Gets the new state of the first light in the LifxLAN object
+        :return: the new power state of the first light as a string
+        """
+        devices = lifx.get_lights()
+        return 'off' if devices[0].get_power() == 0 else "on"
 
-def power_state():
-    """
-    Gets the new state of the first light in the LifxLAN object
-    :return: the new power state of the first light as a string
-    """
-    devices = lifx.get_lights()
-    return 'off' if devices[0].get_power() == 0 else "on"
+    def set_brightness(self, brightness):
+        """
+        Sets the brightness of all lights to the given value
+        :param brightness: the brightness value to set
+        :return: None
+        """
+        self.bright = brightness
+        devices = lifx.get_lights()
+        for device in devices:
+            device.set_brightness(brightness, rapid=True)
+        time.sleep(0.5)
 
+    def get_brightness(self):
+        return self.bright
 
-def set_brightness(brightness):
-    """
-    Sets the brightness of all lights to the given value
-    :param brightness: the brightness value to set
-    :return: None
-    """
-    devices = lifx.get_lights()
-    for device in devices:
-        device.set_brightness(brightness, rapid=True)
-    time.sleep(0.5)
-    print("Light Level : " + format(light_level, '.2f') + " lx")
-
-
-# Toggles power state of all lights based on current state of the first light
-def toggle():
-    """
-    Toggles power state of all lights based on current state of the first light
-    :return: the new, changed power state of the first light as a string
-    """
-    new_state = power_state()
-    on() if power_state() == 'off' else off()
-    return new_state
+    # Toggles power state of all lights based on current state of the first light
+    def toggle(self):
+        """
+        Toggles power state of all lights based on current state of the first light
+        :return: the new, changed power state of the first light as a string
+        """
+        new_state = self.power_state()
+        self.on() if self.power_state() == 'off' else self.off()
+        return new_state
 
 
 if __name__ == "__main__":
-    set_brightness(2500)
+    lifx = Lifx()
+    lifx.set_brightness(2500)
